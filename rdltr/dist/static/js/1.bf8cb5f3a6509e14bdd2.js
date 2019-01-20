42adfc8e43e32064d53514a3e0ef81c7d570b384
Sam <sam@samr1.net>
2019-01-20 13:09:30 +0100

    update .gitignore to keep dist files

---
+++ rdltr/dist/static/js/1.bf8cb5f3a6509e14bdd2.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{YcJa:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("p",[this._v("404 - Not Found")])])}]},r=n("VU/8")(null,l,!1,null,null,null);t.default=r.exports}});
+//# sourceMappingURL=1.bf8cb5f3a6509e14bdd2.js.map
