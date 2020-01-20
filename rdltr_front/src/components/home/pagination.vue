11d547270596b54cd9984fbca83a9dd3f5da1337
Sam <sam@samr1.net>
2020-01-20 10:11:48 +0100

    improve article navigation - fix #18

--- rdltr_front/src/components/home/pagination.vue
+++ rdltr_front/src/components/home/pagination.vue
@@ -1,26 +1,42 @@
 <template>
   <div id="pagination" class="row">
-    <div class="col-1 text-center">
+    <div class="col-md-2 col-sm text-center">
+      <button
+        class="btn-rdltr"
+        type="submit"
+        v-show="displayFirstPage()"
+        @click="getTargetLink(1 - pagination.page)"
+      >
+        <i class="fa fa-angle-double-left " aria-hidden="true"></i>
+      </button>
       <button
         class="btn-rdltr"
         type="submit"
         v-show="pagination.has_prev"
         @click="getTargetLink(-1)"
       >
-        <i class="fa fa-chevron-left" aria-hidden="true" />
+        <i class="fa fa-angle-left" aria-hidden="true"></i>
       </button>
     </div>
-    <div class="col-10 text-center page" v-if="pagination.pages > 0">
+    <div class="col-md-8 col-sm text-center page" v-if="pagination.pages > 0">
       page {{ pagination.page }} / {{ pagination.pages }}
     </div>
-    <div class="col-1 text-center">
+    <div class="col-md-2 col-sm text-center">
       <button
         class="btn-rdltr"
         type="submit"
         v-show="pagination.has_next"
         @click="getTargetLink(1)"
       >
-        <i class="fa fa-chevron-right" aria-hidden="true" />
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+      </button>
+      <button
+        class="btn-rdltr"
+        type="submit"
+        v-show="displayLastPage()"
+        @click="getTargetLink(pagination.pages - pagination.page)"
+      >
+        <i class="fa fa-angle-double-right" aria-hidden="true"></i>
       </button>
     </div>
   </div>
@@ -36,6 +52,12 @@ export default {
     },
   },
   methods: {
+    displayFirstPage() {
+      return this.pagination.page > 2 && this.pagination.pages > 2
+    },
+    displayLastPage() {
+      return this.pagination.pages - 2 >= this.pagination.page
+    },
     getTargetLink(offset) {
       this.$router.push(getTargetLocationFromStore(this.$store.getters, offset))
     },
