0333c9373ba4bd86e1714e16f643cde08036c6a1
Sam <sam@samr1.net>
2023-08-06 15:25:36 +0200

    reset front directory and init project w/ vue3 + pinia + vite

--- rdltr_front/src/App.vue
+++ rdltr_front/src/App.vue
@@ -1,222 +1,85 @@
-<template>
-  <div id="app">
-    <app-header />
-    <router-view />
-  </div>
-</template>
-
-<script>
-import Header from './components/header/header.vue'
-
-export default {
-  name: 'app',
-  components: {
-    'app-header': Header,
-  },
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style src="vue-multiselect/dist/vue-multiselect.min.css" />
-<style>
-body,
-html {
-  color: #4e4e4e;
-  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  margin: 0;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-html {
-  height: 100vh;
-}
-
-body {
-  min-height: 100vh;
-  padding-bottom: 50px;
-  position: relative;
-}
-
-@media screen and (max-width: 400px) {
-  .contnr {
-    width: auto;
-  }
-}
-
-.container {
-  margin-bottom: 1em;
-  margin-top: 0.5em;
-}
-
-.fa {
-  margin-left: 0.3em;
-}
-
-.fa-spinner {
-  color: #8c95aa;
-}
-
-.alert {
-  margin: 0.5em 0;
-}
-
-.article-content a {
-  color: black;
-}
-
-.article-content img,
-.article-content video {
+.logo {
   display: block;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 100%;
+  margin: 0 auto 2rem;
 }
 
-.article-content a > svg {
-  margin: 0 8px 0 -20px;
-  width: 14px;
-}
-
-.article-content pre {
-  background-color: #f4f4f4;
-  padding: 0.5em;
-}
-
-.btn-rdltr {
-  background-color: #f5f5f7;
-  border: 1px solid #8c95aa;
-  color: #8c95aa;
-  cursor: pointer;
-  font: inherit;
-  height: calc(2.25rem + 2px);
-  margin-right: 0.5em;
-  padding: 0.5em 0.8em;
-}
-
-.btn-rdltr:hover {
-  background-color: #8c95aa;
-  color: white;
-}
-
-.btn-rdltr-auto {
+nav {
   width: 100%;
-}
-
-.rdltr-box {
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  margin: 30px auto;
-  padding: 20px;
-  width: 400px;
-}
-
-.rdltr-error {
-  color: red;
+  font-size: 12px;
   text-align: center;
+  margin-top: 2rem;
 }
 
-@media screen and (max-width: 400px) {
-  .rdltr-box {
-    width: auto;
-  }
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-.form-control {
-  border-radius: 0;
-}
-
-.input {
-  margin: 10px auto;
-}
-
-.input label {
-  display: block;
-  margin-bottom: 6px;
-}
-
-.input.inline label {
-  display: inline;
-}
-
-.input input,
-.input textarea {
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  font: inherit;
-  padding: 6px 12px;
-  width: 100%;
-}
-
-.input.inline input {
-  width: auto;
-}
-
-.input input:focus {
-  background-color: #eee;
-  border: 1px solid #8c95aa;
-  outline: none;
-}
-
-.input select {
-  border: 1px solid #ccc;
-  font: inherit;
-}
-
-.link {
-  color: #8c95aa;
-}
-
-.multiselect__input {
-  border-color: black;
-}
-
-.multiselect__option--highlight,
-.multiselect__option--highlight:after {
-  background: #8c95aa;
-  color: #fff;
-}
-
-.multiselect__tag {
-  background-color: #a3aabb;
-}
-
-.multiselect__tag-icon {
-  color: white;
-}
-
-.multiselect__tag-icon:after {
-  color: white;
-}
-
-.multiselect__tag-icon:focus,
-.multiselect__tag-icon:hover {
-  background: #8c95aa;
-}
-
-.multiselect__tags {
-  border: 1px solid #ccc;
-  border-radius: 0;
-}
-
-.submit button {
-  background-color: #f5f5f7;
-  border: 1px solid #8c95aa;
-  font: inherit;
-  color: #8c95aa;
-  cursor: pointer;
-  padding: 10px 20px;
-}
-
-.submit button:hover,
-.submit button:active {
-  background-color: #8c95aa;
-  color: white;
-}
-
-.submit button[disabled],
-.submit button[disabled]:hover,
-.submit button[disabled]:active {
-  border: 1px solid #ccc;
+nav a.router-link-exact-active:hover {
   background-color: transparent;
-  color: #ccc;
-  cursor: not-allowed;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
